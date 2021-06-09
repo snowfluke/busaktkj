8c56fd92a42985331f8d4bae0c930b039b804de7
Awal Ariansyah <57579406+snowfluke@users.noreply.github.com>
2021-06-09 11:19:49 +0700

    Add files via upload

---
+++ hardware/images.map.hardware.js
@@ -0,0 +1,51 @@
+
+export const hardwareImages= {
+
+    "FrontPanel": {
+    	casing: require('./FrontPanel/casing.png'),
+        fpanel: require('./FrontPanel/frontpanel.png'),
+        fpanel2: require ('./FrontPanel/fp.png')
+    },
+
+
+	"BunyiBIOS": {
+		ami: require('./BunyiBIOS/amibios.jpg'),
+		award: require('./BunyiBIOS/awardbios.jpg'),
+		ibm: require('./BunyiBIOS/ibmbios.jpg'),
+		phoenix: require('./BunyiBIOS/phoenixbios.jpg'),
+	},
+
+	"KomponenDasarKomputer": {
+		pc1: require('./KomponenDasarKomputer/keyboard.jpg'),
+		pc2: require('./KomponenDasarKomputer/mouse.jpg'),
+		pc3: require('./KomponenDasarKomputer/touchpad.jpg'),
+		pc4: require('./KomponenDasarKomputer/microphone.jpg'),
+		pc5: require('./KomponenDasarKomputer/webcam.jpg'),
+		pc6: require('./KomponenDasarKomputer/scanner.jpg'),
+		pc7: require('./KomponenDasarKomputer/barcode.jpg'),
+		pc8: require('./KomponenDasarKomputer/joystick.jpg'),
+		pc9: require('./KomponenDasarKomputer/fingerprint.jpg'),
+		pc10: require('./KomponenDasarKomputer/motherboard.jpg'),
+		pc11: require('./KomponenDasarKomputer/processor.jpg'),
+		pc12: require('./KomponenDasarKomputer/ram.jpg'),
+		pc13: require('./KomponenDasarKomputer/vgacard.jpg'),
+		pc14: require('./KomponenDasarKomputer/soundcard.jpg'),
+		pc15: require('./KomponenDasarKomputer/monitor.jpg'),
+		pc16: require('./KomponenDasarKomputer/speaker.jpg'),
+		pc17: require('./KomponenDasarKomputer/printer.jpg'),
+		pc18: require('./KomponenDasarKomputer/proyektor.jpg'),
+		pc19: require('./KomponenDasarKomputer/headset.jpg'),
+	},
+
+	"UrutanKabelUTP": {
+		str: require('./UrutanKabelUTP/straight.jpg'),
+		cross: require('./UrutanKabelUTP/crossover.png'),
+	},
+
+	"JenisKabelJaringan": {
+		coax: require('./JenisKabelJaringan/coaxial.jpg'),
+		fiber: require('./JenisKabelJaringan/fo.jpg'),
+		utp: require('./JenisKabelJaringan/utp.png')
+	},
+
+}
---
+++ software/images.map.software.js
@@ -0,0 +1,52 @@
+
+// Key ip1 dan test2 digunakan di json tag img
+export const softwareImages= {
+	"StrukturDirektoriLinux": {
+		struktur: require('./StrukturDirektoriLinux/struktur-direktori-linux.jpg')
+	},
+
+	"PerintahDasarLinux": {
+		perintah: require('./PerintahDasarLinux/perintah-dasar-man.png')
+	},
+
+	"TextEditorLinux": {
+		editor: require('./TextEditorLinux/shorcuts-nano.png')
+	},
+
+	"KonfigurasiIPAddress": {
+		ip1: require('./KonfigurasiIPAddress/cek-ip-address-1.png'),
+		ip2: require('./KonfigurasiIPAddress/cek-ip-address-2.png')
+	},
+
+	"KonfigurasiRepository": {
+		repo0: require('./KonfigurasiRepository/repositori0.png'),
+		repo1: require('./KonfigurasiRepository/repositori.png'),
+		repo2: require('./KonfigurasiRepository/list-repo.png')
+	},
+
+	"KonfigurasiDNSServer": {
+		dns1: require('./KonfigurasiDNSServer/dns1.png'),
+		dns2: require('./KonfigurasiDNSServer/dns2.png'),
+		dns3: require('./KonfigurasiDNSServer/dns3.png')
+	},
+
+	"KonfigurasiWebServer": {
+		web1: require('./KonfigurasiWebServer/web1.png'),
+		web2: require('./KonfigurasiWebServer/web2.png'),
+		web3: require('./KonfigurasiWebServer/web3.png')
+	},
+
+	"KonfigurasiFTPServer": {
+		ftp1: require('./KonfigurasiFTPServer/ftp1.png'),
+		ftp2: require('./KonfigurasiFTPServer/ftp2.png'),
+	},
+
+	"KonfigurasiNTPServer": {
+		ntp1: require('./KonfigurasiNTPServer/ntp.png'),
+		ntp2: require('./KonfigurasiNTPServer/ntpcek.png'),
+	},
+
+	"KonfigurasiDHCPServer": {
+		dhcp: require('./KonfigurasiDHCPServer/dhcp.png')
+	},
+}
